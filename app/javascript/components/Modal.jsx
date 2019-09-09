cff9064bf402dc49d3179ab0ab8f7d23699e26a5
Julio Lopez <julioj.lopeza@gmail.com>
2019-09-09 02:40:27 -0300

    modal component

---
+++ app/javascript/components/Modal.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { MDBContainer, MDBBtn, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter } from 'mdbreact';
+
+class ModalPage extends Component {
+
+render() {
+    const { open, handleClose } = this.props
+  return (
+      <MDBContainer>
+        <MDBBtn color="primary" onClick={handleClose}>MDBModal</MDBBtn>
+        <MDBModal isOpen={open} toggle={handleClose} centered>
+          <MDBModalHeader toggle={handleClose}>Notification</MDBModalHeader>
+          <MDBModalBody>
+            {this.props.children}
+          </MDBModalBody>
+          <MDBModalFooter>
+            <MDBBtn color="secondary" onClick={handleClose}>Close</MDBBtn>
+          </MDBModalFooter>
+        </MDBModal>
+      </MDBContainer>
+    );
+  }
+}
+
+export default ModalPage;
