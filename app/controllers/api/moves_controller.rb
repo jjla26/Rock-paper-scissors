b968465b99bd5698cdff1b6103a51fea1a9c5f32
Julio Lopez <julioj.lopeza@gmail.com>
2019-09-09 02:40:15 -0300

    updating and creating movements

--- app/controllers/api/moves_controller.rb
+++ app/controllers/api/moves_controller.rb
@@ -1,6 +1,33 @@
 class Api::MovesController < ApplicationController
+    skip_before_action :verify_authenticity_token
+
     def index
         moves = Move.all
         render json: moves
     end
+
+    def update
+        move = Move.find(params[:id])
+        if move.update(move_params)
+            render json: move
+        else
+            render json: { status: 401 }
+        end
+    end
+
+    def create
+        move = Move.new(move_params)
+        if move.save
+            render json: move
+        else
+            render json: { status: 401 }
+        end
+    end
+
+    private
+
+    def move_params
+        params.permit(:move, :kills, :active)
+    end
+
 end
--- app/javascript/containers/Configuration.jsx
+++ app/javascript/containers/Configuration.jsx
@@ -2,13 +2,23 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { MDBContainer, MDBDataTable, MDBRow, MDBCol, MDBInput, MDBBtn, MDBJumbotron, MDBSelect } from 'mdbreact';
 import Spinner from '../components/Spinner'
-import { read } from '../services/api'
+import ConfigurationForm from '../components/ConfigurationForm'
+import Modal from '../components/Modal'
+import { read, update, create } from '../services/api'
 
 class Configuration extends React.Component{
 
     state={
+        form:{
+            move:"",
+            kills:"",
+        },
         moves: [],
         loading: true,
+        open:false,
+        openForm: false,
+        error:false,
+        message: "",
     }
 
     async componentDidMount(){
@@ -16,8 +26,55 @@ class Configuration extends React.Component{
         this.setState({ ...this.state, moves, loading: false })
     }
 
+    handleClose = () =>{
+        this.setState({ ...this.state, open: false, openForm: false})
+    }
+
+    handleActive = id => async e => {
+        const { moves } = this.state
+        const move = moves.filter(x => x.id === id)[0]
+        try{
+            const moveUpdated = await update('/moves', id, {...move, active: !move.active})
+            this.setState({ ...this.state, moves: moves.map(x => {if(x.id === id){return moveUpdated}else{return x}}) })
+        }
+        catch{
+            this.setState({ ...this.setState, open: true, message: "Updating error, contact the admin"})
+        }
+    } 
+
+    handleFormModal = e => {
+        console.log("modal")
+      this.setState({ ...this.state, openForm: true})  
+    }
+
+    handleChange = field => e => {
+        this.setState({
+            ...this.state,
+            form:{
+                ...this.state.form,
+                [field]: e.target.value
+            }
+        })
+    }
+
+    handleSubmitConfiguration = async e => {
+        const { form, moves } = this.state
+        e.preventDefault()
+        if(form.move === "" || form.kills === ""){
+            this.setState({ ...this.state, error: true, message: "all the fields are required!"})
+        }else{
+            try {
+                const newMove = await create('/moves', form)
+                this.setState({ ...this.state, openForm: false, open: true, message:"Move Created", moves: moves.concat([newMove])})
+            } catch (error) {
+                this.setState({ ...this.setState, openForm:false, open: true, message: "Creating error, contact the admin"})
+            }
+        }
+    }
+
     render(){
-        const { loading, moves } = this.state 
+        const { loading, moves, open, message, error, openForm } = this.state
+        console.log(this.state) 
         return(
             loading ? <Spinner /> :
             <MDBContainer className="my-5 text-center">
@@ -29,7 +86,7 @@ class Configuration extends React.Component{
                     </MDBRow>
                     <MDBRow center middle>
                         <MDBCol md="12">
-                            <MDBBtn>Add Move</MDBBtn>
+                            <MDBBtn onClick={this.handleFormModal}>Add Move</MDBBtn>
                             <Link to="/">
                                 <MDBBtn>Back to Home</MDBBtn>
                             </Link>
@@ -76,7 +133,7 @@ class Configuration extends React.Component{
                                         active: x.active ? "YES" : "NO",
                                         action: <div>
                                                     <MDBBtn>Edit</MDBBtn>
-                                                    <MDBBtn>Delete</MDBBtn>
+                                                    <MDBBtn onClick={this.handleActive(x.id)}>{x.active ? "Deactivate" : "Activate" }</MDBBtn>
                                                 </div>
                                     }))
                                 }}
@@ -84,6 +141,21 @@ class Configuration extends React.Component{
                         </MDBCol>
                     </MDBRow>
                 </MDBJumbotron>
+
+                <Modal
+                    handleClose={this.handleClose}
+                    open={open}
+                >
+                    {message}
+                </Modal>
+
+                <Modal
+                    handleClose={this.handleClose}
+                    open={openForm}
+                >
+                    <ConfigurationForm error={error} message={message} handleSubmitConfiguration={this.handleSubmitConfiguration} handleChange={this.handleChange}/>
+
+                </Modal>
             </MDBContainer>
 
         )
